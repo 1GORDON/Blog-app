dfa95eefb2d3bd7a2db7d9cc2fdade990e99dde4
Gordon Tinyefuza <gordon.tinyefuza@gmail.com>
2022-02-17 23:53:07 +0300

    Add posts_spec request tests

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,6 +2,34 @@ require 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    before(:example) { get user_posts_path(10) }
+
+    it 'should return http 200 success' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'should render the index template' do
+      expect(response).to render_template('index')
+    end
+
+    it 'should render the correct text in the template' do
+      expect(response.body).to include('Here is a list of posts for a given user')
+    end
+  end
+
+  describe 'GET /show' do
+    before(:example) { get user_posts_path(10,10) }
+
+    it 'should return a correct response' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'should render the show template' do
+      expect(response).to render_template('show')
+    end
+
+    it 'should render the correct template text' do
+      expect(response.body).to include('Here is a display of a given post for a given user')
+    end
   end
 end
