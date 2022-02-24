c1bf6c6d83ecf118cb524c9649b64926ca1590bd
Gordon Tinyefuza <gordon.tinyefuza@gmail.com>
2022-02-24 15:41:49 +0300

    Create first and second user, first_post and create post comments

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,11 +2,11 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :user_id
   belongs_to :post
 
-  after_save :update_likes_counter
+#   after_save :update_likes_counter
 
-  private
+#   private
 
-  def update_likes_counter
-    post.increment!(:likes_counter)
-  end
+#   def update_likes_counter
+#     post.increment!(:likes_counter)
+#   end
 end
