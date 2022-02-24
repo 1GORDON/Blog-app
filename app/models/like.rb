8ab3973bd29d78b924baa0841ead0435c735ecd5
Gordon Tinyefuza <gordon.tinyefuza@gmail.com>
2022-02-25 00:59:19 +0300

    Update like model file

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,11 +2,9 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  after_save :update_likes_counter
+  after_save :update_like_counter
 
-  private
-
-  def update_likes_counter
+  def update_like_counter
     post.increment!(:likes_counter)
   end
 end
