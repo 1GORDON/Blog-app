72500306879d771516d73b374a97b55390156121
Gordon Tinyefuza <gordon.tinyefuza@gmail.com>
2022-02-26 00:36:45 +0300

    Update models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,10 @@
 class Comment < ApplicationRecord
-  belongs_to :user
-  belongs_to :post
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
 
-  after_save :update_comment_counter
-
-  def update_comment_counter
-    post.increment!(:comments_counter)
+  def self.count_comments(id)
+    post = Post.find(id)
+    post.commentsCounter = Comment.where(post_id: id).count
+    post.save
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,19 @@
 class Post < ApplicationRecord
-  belongs_to :user
-  has_many :comments
-  has_many :likes
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+  has_many :comments, class_name: 'Comment', foreign_key: 'post_id'
+  has_many :likes, class_name: 'Like', foreign_key: 'post_id'
 
-  def show_recent_5_comments
-    comments.limit(5).order(created_at: :desc)
+  def self.recent_comments(id)
+    Comment.where(post_id: id).limit(5)
   end
 
-  after_save :update_post_counter
+  def self.all_comments(id)
+    Comment.where(post_id: id)
+  end
 
-  def update_post_counter
-    user.increment!(:posts_counter)
+  def self.count_post(id)
+    user = User.find(id)
+    user.postsCounter = Post.where(user_id: id).count
+    user.save
   end
 end
