85ca28ad1c904bcad2a09a881e453f657c47e375
Gordon Tinyefuza <gordon.tinyefuza@gmail.com>
2022-03-05 21:15:14 +0300

    Update routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
+  namespace :api , defaults: { format: :json } do
+    namespace :v1 do
+      post 'users/sign_in' => 'users#login'
+      get 'posts' => 'posts#index'
+      get 'comments' => 'comments#index'
+      post 'comments/create' => 'comments#create'
+    end
+  end
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show new create] do
-      resources :comments, only: [:create]
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create, :destroy] do
+      resources :comments, only: [:create, :destroy]
       resources :likes, only: [:create]
     end
   end
