18ae4697ac8e5d1094a152b9c7deb3fed78bf0c8
Wes-Isaac <yisehakmitiku@gmail.com>
2022-03-06 03:31:51 +0800

    Fix linter issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  namespace :api , defaults: { format: :json } do
+  namespace :api, defaults: { format: :json } do
     namespace :v1 do
       post 'users/sign_in' => 'users#login'
       get 'posts' => 'posts#index'
@@ -11,9 +11,9 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create, :destroy] do
-      resources :comments, only: [:create, :destroy]
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index show new create destroy] do
+      resources :comments, only: %i[create destroy]
       resources :likes, only: [:create]
     end
   end
